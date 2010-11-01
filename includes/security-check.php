519517032ef3993915105c691f526920939fa0d7
Dave Miller <mercurial@dave-miller.com>
2010-11-01 20:41:19 +0000

    Initial import

---
+++ includes/security-check.php
@@ -0,0 +1,22 @@
+<?php
+
+if (PHP_SAPI != 'cli')
+{
+  if (ini_get('register_globals'))
+  {
+    die("Error: register_globals is on\n");
+  }
+  elseif (ini_get('magic_quotes_gpc'))
+  {
+    die("Error: magic_quotes_gpc is on\n");
+  }
+  elseif (ini_get('magic_quotes_runtime'))
+  {
+    die("Error: magic_quotes_runtime is on\n");
+  }
+  elseif (ini_get('magic_quotes_sybase'))
+  {
+    die("Error: magic_quotes_sybase is on\n");
+  }
+}
+
